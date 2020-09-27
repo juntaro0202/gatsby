c7a70129640623af37abc4342990f47cec50d455
juntaro0202 <zyunn14641@gmail.com>
2020-09-27 11:16:30 +0900

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,155 @@
+import React from "react"
+
+export default () => (
+  <div>
+    <div>
+      <header classname="header">
+        <div classname="container">
+          <div classname="site">
+            <a href="base-index.html">
+              <img src="/images/logo.svg" alt="ESSENTIALS" />
+            </a>
+          </div>
+          <nav classname="nav">
+            <ul>
+              <li>
+                <a href="base-index.html">TOP</a>
+              </li>
+              <li>
+                <a href="base-about.html">ABOUT</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+      <section classname="hero">
+        <figure>
+          <img src="/images/hero.jpg" alt="" />
+        </figure>
+        <div classname="catch">
+          <h1>
+            There is no love sincerer than
+            <br />
+            the love of food.
+          </h1>
+          <p>食物を愛するよりも誠実な愛はない ― バーナード・ショー</p>
+        </div>
+        <div classname="wave">
+          <img src="/images/wave.svg" alt="" />
+        </div>
+      </section>
+      <section classname="food">
+        <div classname="container">
+          <h2 classname="bar">
+            Food <span>Essence</span>
+          </h2>
+          <div classname="details">
+            <div classname="detail">
+              <figure>
+                <img src="/images/fruit.jpg" alt="" />
+              </figure>
+              <h3>フルーツ</h3>
+              <p>FRUIT</p>
+              <p>
+                甘くてすっぱくておいしい果実たち。
+                <br />
+                旬のフルーツを満喫します。
+              </p>
+            </div>
+            <div classname="detail">
+              <figure>
+                <img src="/images/grain.jpg" alt="" />
+              </figure>
+              <h3>穀物</h3>
+              <p>GRAIN</p>
+              <p>
+                食事の基本となる穀物。
+                <br />
+                毎日の活動のエネルギー源になります。
+              </p>
+            </div>
+            <div classname="detail">
+              <figure>
+                <img src="/images/beverage.jpg" alt="" />
+              </figure>
+              <h3>飲み物</h3>
+              <p>BEVERAGE</p>
+              <p>
+                リラックスするのに欠かせない飲み物。
+                <br />
+                お気に入りの一杯はありますか？
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section classname="photo">
+        <h2 classname="sr-only">Photo</h2>
+        <figure>
+          <img src="/images/berry.jpg" alt="赤く熟したベリー" />
+        </figure>
+      </section>
+      <footer classname="footer">
+        <div classname="container">
+          <div classname="site">
+            <a href="base-index.html">
+              <img src="/images/logo-w.svg" alt="ESSENTIALS" />
+              <p>おいしい食材と食事を探求するサイト</p>
+            </a>
+          </div>
+          <ul classname="sns">
+            <li>
+              <a href="https://twitter.com/">
+                <i classname="fab fa-twitter">
+                  <span classname="sr-only">Twitter</span>
+                </i>
+              </a>
+              <i classname="fab fa-twitter"></i>
+            </li>
+            <i classname="fab fa-twitter">
+              <li>
+                <a href="https://facebook.com/">
+                  <i classname="fab fa-facebook-square">
+                    <span classname="sr-only">Facebook</span>
+                  </i>
+                </a>
+                <i classname="fab fa-facebook-square"></i>
+              </li>
+              <i classname="fab fa-facebook-square">
+                <li>
+                  <a href="http://instagram.com/">
+                    <i classname="fab fa-instagram">
+                      <span classname="sr-only">Instagram</span>
+                    </i>
+                  </a>
+                  <i classname="fab fa-instagram"></i>
+                </li>
+                <i classname="fab fa-instagram"></i>
+              </i>
+            </i>
+          </ul>
+          <i classname="fab fa-twitter">
+            <i classname="fab fa-facebook-square">
+              <i classname="fab fa-instagram"></i>
+            </i>
+          </i>
+        </div>
+        <i classname="fab fa-twitter">
+          <i classname="fab fa-facebook-square">
+            <i classname="fab fa-instagram"></i>
+          </i>
+        </i>
+      </footer>
+      <i classname="fab fa-twitter">
+        <i classname="fab fa-facebook-square">
+          <i classname="fab fa-instagram"></i>
+        </i>
+      </i>
+    </div>
+    <i classname="fab fa-twitter">
+      <i classname="fab fa-facebook-square">
+        <i classname="fab fa-instagram"></i>
+      </i>
+    </i>
+  </div>
+)
